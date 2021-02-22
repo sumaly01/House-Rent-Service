eafcf2e290402c00f72500963a6997edc625af13
lisha <mhzlysa@gmail.com>
2021-02-23 00:05:55 +0545

    waterSupply type changed

--- client/src/app/models/property.model.ts
+++ client/src/app/models/property.model.ts
@@ -1,22 +1,19 @@
 export class Property {
-    _id: string;
-    contact_person_name: string;
-    state: string;
-    city: string;
-    property_type: string;
-    address: string;
-    mobile_number: string;
-    email: string;
-    price: number;
-    images: [string];
-    description: string;
-    numberOfRooms: number;
-    waterSupply: string;
-    parking: boolean;
-    numberOfBathrooms: number;
-    lat: number;
-    lng: number
-
+  _id: string;
+  contact_person_name: string;
+  state: string;
+  city: string;
+  property_type: string;
+  address: string;
+  mobile_number: string;
+  email: string;
+  price: number;
+  images: [string];
+  description: string;
+  numberOfRooms: number;
+  waterSupply: boolean;
+  parking: boolean;
+  numberOfBathrooms: number;
+  lat: number;
+  lng: number;
 }
-
-
