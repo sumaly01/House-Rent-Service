58391f9bf81036112e99a1e652e5453a3d4bcd6a
Buddhi Raj Paudel <brajpaudel07@gmail.com>
2021-02-23 22:20:51 +0545

    reset password

--- client/src/app/auth/reset-password/reset-password.component.ts
+++ client/src/app/auth/reset-password/reset-password.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-reset-password',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reset-password.component.scss']
 })
 export class ResetPasswordComponent implements OnInit {
-
-  constructor() { }
+  user
+  constructor(private authService: AuthService, public toastr: ToastrService, public router: Router) {
+    this.user = {}
+  }
 
   ngOnInit() {
+  }
+
+  resetPassword() {
+    this.authService.resetPass(this.user).subscribe((response: any) => {
+      this.router.navigate(['/auth/login']);
+      this.toastr.success('Your password has been reset successfully !')
+      localStorage.clear();
+
+    })
+
   }
 
 }
